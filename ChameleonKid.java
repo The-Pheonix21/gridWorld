87b99245866864c0da9c91750bef765cf0b9230a
Chris <c5keith90@gmail.com>
2014-01-30 08:08:19 -0700

    finished

--- ChameleonKid.java
+++ ChameleonKid.java
@@ -10,35 +10,14 @@ import java.util.ArrayList;
 public class ChameleonKid extends ChameleonCritter 
 { 
  
- public ArrayList<Actor> getActors() 
- { 
- ArrayList<Actor> actors = new ArrayList<Actor>(); 
- int[] dirs = 
- { Location.AHEAD, Location.HALF_CIRCLE }; 
- for (Location loc : getLocationsInDirections(dirs)) 
- { 
- Actor a = getGrid().get(loc); 
- if (a != null) 
- actors.add(a); 
+public ArrayList<Actor> getActors() { 
+     ArrayList<Actor> actors = new ArrayList<Actor>(); 
+     int[] dirs = { Location.AHEAD, Location.HALF_CIRCLE }; 
+         for (Location loc : getLocationsInDirections(dirs)) { 
+            Actor a = getGrid().get(loc); 
+            if (a != null) 
+            actors.add(a); 
  } 
  
  return actors; 
- } Part 4 
- 
- 
-
- public ArrayList<Location> getLocationsInDirections(int[] directions) 
- { 
- ArrayList<Location> locs = new ArrayList<Location>(); 
- Grid gr = getGrid(); 
- Location loc = getLocation(); 
- 
- for (int d : directions) 
- { 
- Location neighborLoc = loc.getAdjacentLocation(getDirection() + d); 
- if (gr.isValid(neighborLoc)) 
- locs.add(neighborLoc); 
  } 
- return locs; 
- } 
-} 
