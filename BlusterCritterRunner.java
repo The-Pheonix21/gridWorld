d5cbc2c7d3379d54474af4b28cb1f3cd8208b4cf
Chris <c5keith90@gmail.com>
2014-02-06 10:04:53 -0700

    ish

--- BlusterCritterRunner.java
+++ BlusterCritterRunner.java
@@ -1,5 +1,5 @@
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.actor.Critter;
+import info.gridworld.actor.BlusterCritter;
 import info.gridworld.actor.Flower;
 import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
@@ -10,7 +10,7 @@ import java.awt.Color;
  * This class runs a world that contains critters. <br />
  * This class is not tested on the AP CS A and AB exams.
  */
-public class BlusterCritterRunner
+public class BlusterCritterRunner 
 {
     public static void main(String[] args)
     {
@@ -21,8 +21,8 @@ public class BlusterCritterRunner
         world.add(new Location(5, 5), new Flower(Color.PINK));
         world.add(new Location(1, 5), new Flower(Color.RED));
         world.add(new Location(7, 2), new Flower(Color.YELLOW));
-        world.add(new Location(4, 4), new Critter());
-        world.add(new Location(5, 8), new Critter());
+        world.add(new Location(4, 4), new BlusterCritter(1));
+        world.add(new Location(5, 8), new BlusterCritter(9));
         world.show();
     }
 }
--- RockHoundRunner.java
+++ RockHoundRunner.java
@@ -6,10 +6,7 @@ import info.gridworld.grid.Location;
 
 import java.awt.Color;
 
-/**
- * This class runs a world that contains critters. <br />
- * This class is not tested on the AP CS A and AB exams.
- */
+
 public class RockHoundRunner
 {
     public static void main(String[] args)
